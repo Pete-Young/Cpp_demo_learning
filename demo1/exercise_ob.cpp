7a1e5a435ea6aad36f276e9ca625fd81de61553e
Pete-Young <yangrui5677817@163.com>
2025-05-28 23:38:33 +0800

    add xigouy

--- demo1/exercise_ob.cpp
+++ demo1/exercise_ob.cpp
@@ -1,8 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 using namespace std;
 
-// �����������
+// 立方体类设计
 class Cube
 {
 public:
@@ -63,6 +63,28 @@ bool isSame(Cube& c1, Cube& c2)
 
 
 
+/*  #### 4.2.1 构造函数和析构函数
+
+对象的**初始化和清理**也是两个非常重要的安全问题
+
+​	一个对象或者变量没有初始状态，对其使用后果是未知
+
+​	同样的使用完一个对象或变量，没有及时清理，也会造成一定的安全问题
+
+
+
+c++利用了**构造函数**和**析构函数**解决上述问题，这两个函数将会被编译器自动调用，完成对象初始化和清理工作。
+
+对象的初始化和清理工作是编译器强制要我们做的事情，因此如果**我们不提供构造和析构，编译器会提供**
+
+**编译器提供的构造函数和析构函数是空实现。**
+
+
+
+* 构造函数：主要作用在于创建对象时为对象的成员属性赋值，构造函数由编译器自动调用，无须手动调用。
+* 析构函数：主要作用在于对象**销毁前**系统自动调用，执行一些清理工作。
+*/
+
 int main()
 {
 	Cube c1;
@@ -70,9 +92,9 @@ int main()
 	c1.setW(10);
 	c1.setH(10);
 
-	cout << "c1�����" << c1.calculateS() << endl;
+	cout << "c1的面积" << c1.calculateS() << endl;
 
-	// �����ڶ���������
+	// 创建第二个立方体
 	Cube c2;
 	c2.setL(10);
 	c2.setW(10);
@@ -81,7 +103,7 @@ int main()
 	bool ret = isSame(c1, c2);
 		cout << ret << endl;
 
-	// ���ó�Ա�����ж�
+	// 利用成员函数判断
 	bool ret2 = c1.isSameByClass(c2);
 	cout << ret2 << endl;
 
