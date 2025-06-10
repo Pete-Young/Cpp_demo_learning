7b603c7147d9bbe72b06fe37d7783d1381c693f3
Pete-Young <yangrui5677817@163.com>
2025-06-10 23:24:22 +0800

    add duotai

---
+++ demo2/demo2/inheritance.cpp
@@ -0,0 +1,30 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//// 继承实现   class 子类；继承方式：父类
+//class BasePage
+//{
+//public:
+//	void header()
+//	{
+//		cout << "header" << endl;
+//	}
+//};
+//class Java :public BasePage
+//{
+//public:
+//	void content()
+//	{
+//		cout << "java video" << endl;
+//	}
+//};
+//void test01()
+//{
+//	Java ja;
+//	ja.header();
+//}
+//int main()
+//{
+//	test01();
+//	return 0;
+//}
